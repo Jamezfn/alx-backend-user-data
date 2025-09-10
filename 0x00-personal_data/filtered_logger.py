d673b20afc7ffbcce4ea3976a2abf2a411dd4e6c
Jamezfn <jamesmakachia19@gmail.com>
2025-09-10 05:10:33 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
-"""Regex-ing"""
+"""
+Personal data
+"""
 import re
-import logging
 
 def filter_datum(fields, redaction, message, separator):
-    """Returns the log message obfuscated"""
-    pattern = f"({'|'.join(fields)}=[^{separator}]*)"
+    """Regex-ing"""
+    pattern = rf"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
