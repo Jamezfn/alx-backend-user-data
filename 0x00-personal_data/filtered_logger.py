99c5648e2a2bb59c69a9bed4933f2aa440838205
Jamezfn <jamesmakachia19@gmail.com>
2025-09-11 19:40:34 +0300

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,11 @@
 """
 Personal data
 """
+import os
 import re
 import logging
+import mysql.connector
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -25,7 +28,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage())
+        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
         return super().format(record)
 
 def get_logger() -> logging.Logger:
@@ -38,3 +41,31 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Get a database connection using environment variables"""
+    return mysql.connector.connect(
+            user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+            password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+            host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+            database=os.getenv("PERSONAL_DATA_DB_NAME", "")
+    )
+
+def main():
+    """Obtain a database connection, query users, and log each row"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    logger = get_logger()
+    fields: List[str] = cursor.column_names
+
+    for row in cursor:
+        message = ";".join(f"{field}={value}" for field, value in zip(fields, row))
+        logger.info(message + ";")
+
+    cursor.close()
+    db.close()
+
+if __name__ == '__main__':
+    main()
