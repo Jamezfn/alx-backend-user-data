5a08653ce667ccd359cd319056e8b1f380477494
Jamezfn <jamesmakachia19@gmail.com>
2025-09-29 13:14:29 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,10 @@ class Auth:
         """
         if request is None:
             return None
-        return None
+
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns the current user
