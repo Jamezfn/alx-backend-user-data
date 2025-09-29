496784f7a28755e2b49d343e4a0fa5d78c42bca6
Jamezfn <jamesmakachia19@gmail.com>
2025-09-29 16:53:18 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,9 @@
 """
 
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
+from typing import TypeVar
 
 class BasicAuth(Auth):
     """ BasicAuth inherits from Auth """
@@ -30,3 +32,24 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """"""
+        if user_email is None or  not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or  not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+        except DatabaseError as e:
+            return None
+
+        if not users or len(users) == 0:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
