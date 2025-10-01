e1497e3dd21ca1e8d81acb3461d61cf00bdaf315
Jamezfn <jamesmakachia19@gmail.com>
2025-10-01 18:09:20 +0300

    2. Find user

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -2,3 +2,4 @@ User authentication service
 
 0. User model
 1. create user
+2. Find user
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,6 +3,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -34,3 +36,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find the first user matching the keyword arguments"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
