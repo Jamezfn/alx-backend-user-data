0ed5da0d09bf398508ae8b3b3d52e55f1565715d
Jamezfn <jamesmakachia19@gmail.com>
2025-10-02 13:37:34 +0300

    5. Register user and 6. Basic Flask app

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -4,3 +4,5 @@ User authentication service
 1. create user
 2. Find user
 3. update user
+4. Hash password
+5. Register user
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,25 @@
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_pwd)
+            return new_user
